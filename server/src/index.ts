4de3355170ab3b0fb11e23ead975b9f413549473
Lucas Duarte <lucas@pop-os.localdomain>
2020-09-30 17:14:36 -0300

    Add promise reject handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,16 @@
 import { ServerSetup } from './server'
 import config from 'config'
 
+process.on('unhandledRejection', (reason, promise) => {
+  console.error(
+    `App exiting due to an unhandled promise: ${promise} and reason: ${reason}`
+  )
+  // lets throw the error and let the uncaughtException handle below handle it
+  throw reason
+});
+
 (async (): Promise<void> => {
   const server = new ServerSetup(config.get('App.port'))
   await server.init()
-  await server.start()
+  server.start()
 })()
